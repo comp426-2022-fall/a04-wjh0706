a4940c6d9118952d2710f8c36f0bdad41a349f9b
Jianghao Wu <62315764+wjh0706@users.noreply.github.com>
2022-11-29 22:42:01 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,35 +10,24 @@ app.get('/app/',function (req, res) {
 	res.status(200).send('200 OK');
 })
 
-
-
 app.get('/app/roll/', function(req, res) {
 	res.status(200).send(roll(6, 2, 1))
 })
 
 app.post('/app/roll/', function(req, res) {
-	const sides = Number(req.body.sides)
-	const dice = Number(req.body.dice)
-	const rolls = Number(req.body.rolls)
-	res.status(200).send(roll(sides, dice, rolls))
+	res.status(200).send(roll(Number(req.body.sides), Number(req.body.dice), Number(req.body.rolls)))
 })
 
 app.get('/app/roll/:sides/', function(req, res) {
-	const sides = Number(req.params.sides)	
-	res.status(200).send(roll(sides, 2, 1))
+res.status(200).send(roll(Number(req.body.sides), 2, 1))
 })
 
 app.get('/app/roll/:sides/:dice/', function(req, res) {
-	const sides = Number(req.params.sides)
-	const dice = Number(req.params.dice)	
-	res.status(200).send(roll(sides, dice, 1))
+	res.status(200).send(roll(Number(req.body.sides), Number(req.body.dice), 1))
 })
 
 app.get('/app/roll/:sides/:dice/:rolls/', function(req, res) {
-	const sides = Number(req.params.sides)
-	const dice = Number(req.params.dice)
-	const rolls = Number(req.params.rolls)
-	res.status(200).send(roll(sides, dice, rolls))
+	res.status(200).send(roll(Number(req.body.sides), Number(req.body.dice), Number(req.body.rolls)))
 })
 
 app.get('*', function(req, res) {
